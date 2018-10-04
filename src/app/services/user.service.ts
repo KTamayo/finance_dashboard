bf179e32edca7bccdd7292124ac95f2eb7a22572
Karlo Tamayo <karlo.tamayo.1010@gmail.com>
2018-10-03 19:27:58 -0700

    Changed API url from cloud9 server for local development, need to make variable urls for local and deployed versions

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,25 +9,26 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class UserService {
 
   constructor(private http: HttpClient, private router: Router) { }
-  
+
   userData:any;
   loggedIn:boolean;
   token = window.sessionStorage.getItem("token");
   userId = window.sessionStorage.getItem("userId");
-  
-  apiBaseUrl = 'http://mean-stack-charlie-2018-karlo-phortonssf.c9users.io:8080/api'
+
+  // apiBaseUrl = 'http://mean-stack-charlie-2018-karlo-phortonssf.c9users.io:8080/api'
+  apiBaseUrl = 'http://localhost:5000/api'
   registrationUrl = `${this.apiBaseUrl}/appUsers`;
   loginUrl = `${this.apiBaseUrl}/appUsers/login`;
   logoutUrl = `${this.apiBaseUrl}/appUsers/logout?access_token=${this.token}`;
-  
+
   register(user){
     return this.http.post(this.registrationUrl, user);
   }
-  
+
   login(user){
     return this.http.post(this.loginUrl, user);
   }
-  
+
   logout(){
     // this.token = window.sessionStorage.getItem("token");
     // this.logoutUrl = `${this.apiBaseUrl}/appUsers/logout?access_token=${this.token}`;
@@ -37,13 +38,13 @@ export class UserService {
     window.sessionStorage.removeItem("userId");
     this.router.navigate(["login"]);
   }
-  
+
   toHomePage(resData){
     window.sessionStorage.setItem("token", resData.token);
     window.sessionStorage.setItem("userId", resData.userId);
     this.router.navigate(["home"]);
   }
-  
+
   getUserData(){
     this.token = window.sessionStorage.getItem("token");
     this.userId = window.sessionStorage.getItem("userId");
